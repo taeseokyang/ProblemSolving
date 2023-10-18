b521ee027245b6ad4452b29a1617d5c14fe62d3d
taeseokyang <ts.yang.0123@gmail.com>
2023-10-18 23:48:18 +0900

    10/18

---
+++ baekjoon/java/dp/p11722.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    static int dp[] = new int[1000];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int lds = 0;
+        String datas[] = br.readLine().split(" ");
+        for(int i=n-1;i>=0;i--) {
+            int data = Integer.parseInt(datas[i]);
+            int idx = bs(0,lds,data);
+            if (idx >= lds ) lds++;
+            dp[idx] = data;
+        }
+        System.out.print(lds);
+    }
+    static int bs(int l, int r, int k){
+        int m;
+        while(l<r){
+            m = (l+r)/2;
+            if(dp[m]<k)l = m+1;
+            else r = m;
+        }
+        return r;
+    }
+}
